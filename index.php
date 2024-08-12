da6b5c383ca945ba25cc1687ebf8dafbe5231865
Debby Lestari <debiseptialestari@gmail.com>
2024-08-12 12:08:38 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Debby Septya</title>
+  </head>
+  <body>
+    <h1>Halo Teman-Teman Semuanya</h1>
+    <nav>
+      <ul>
+        <li><a href="#Home">Home</a></li>
+        
+  </body>
+</html>
